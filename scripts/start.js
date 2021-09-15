2bce12ebd4531b219eee52697d8ecbb2deb2fc38
John Yuan <john.ycq@outlook.com>
2021-09-16 00:12:43 +0800

    feat: sync build process

--- scripts/start.js
+++ scripts/start.js
@@ -9,24 +9,47 @@ const debounce = require('lodash/debounce')
 const express = require('express')
 const server = express()
 
-const build = debounce(() => {
-  childProcess.exec('npm run build', (error, stdout, stderr) => {
-    if (error) {
-      console.error(error)
-    }
+const runCommand = () => {
+  return new Promise((resolve) => {
+    childProcess.exec('npm run build', (error, stdout, stderr) => {
+      if (error) {
+        console.error(error)
+      }
 
-    if (stdout) {
-      console.log(stdout)
-    }
+      if (stdout) {
+        console.log(stdout)
+      }
 
-    if (stderr) {
-      console.error(stderr)
-    }
+      if (stderr) {
+        console.error(stderr)
+      }
 
-    console.log(`Build at ${new Date().toISOString()}`)
-    console.log(`http://localhost:${PORT}/`)
-    console.log()
+      console.log(`Build at ${new Date().toISOString()}`)
+      console.log(`http://localhost:${PORT}/`)
+      console.log()
+
+      resolve()
+    })
   })
+}
+
+const state = { building: false, changed: false }
+
+const build = debounce(() => {
+  if (state.building) {
+    state.changed = true
+  } else {
+    state.changed = false
+    state.building = true
+
+    runCommand().then(() => {
+      state.building = false
+
+      if (state.changed) {
+        build()
+      }
+    })
+  }
 }, 500)
 
 process.chdir(ROOT_DIR)
